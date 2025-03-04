2fe611a9c3eaab8826c347e01e7ce7133717b66f
tasfiahm <38252728+tasfiahm@users.noreply.github.com>
2025-03-05 02:17:18 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ ingredients_list  = st.multiselect("Choose up to 5 ingrenents:", my_dataframe, m
 
 # Display selected options
 if ingredients_list:
-    st.write(ingredients_list)
+ #   st.write(ingredients_list)
     st.text(ingredients_list)
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
