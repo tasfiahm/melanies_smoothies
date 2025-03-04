3031ef49da6af2aae81470147fd6e533b796c996
tasfiahm <38252728+tasfiahm@users.noreply.github.com>
2025-03-05 04:51:29 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ cnx = st.connection("snowflake") #adding snowflake session
 session = cnx.session() #adding snowflake session
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 # Define options
 #options = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
 
