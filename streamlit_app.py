1681b681dfe4b7f5d3d0396bca5c7e9dabbaa14a
tasfiahm <38252728+tasfiahm@users.noreply.github.com>
2025-03-06 00:31:58 +0600

    Update streamlit_app.py
    Adding :search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #Convert the snowflake dataframe to a pandas dataframe so we can use the loc funtion
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 # Define options
 #options = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
 
@@ -59,6 +59,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
